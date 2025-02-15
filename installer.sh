778073be856260a3114cd138bfc138cb876147ff
Vinicius Mayrink <vmayrink12@gmail.com>
2025-02-15 11:51:05 -0300

    chore(installer): adds steps for installing bin setup and default scripts

--- installer.sh
+++ installer.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-curl -L https://github.com/vncsmyrnk/shell-utils/raw/refs/heads/main/bin/util.sh -o /tmp/util
-sudo install /tmp/util /usr/local/bin
+curl -L https://github.com/vncsmyrnk/shell-utils/archive/refs/heads/main.zip -o /tmp/shell-utils.zip
+unzip /tmp/shell-utils.zip -d /tmp
+\. /tmp/shell-utils-main/config/setup
+sudo install /tmp/shell-utils-main/bin/util $UTILS_BIN_PATH
+install /tmp/shell-utils-main/config/setup $UTILS_PATH
+mkdir -p $UTILS_SCRIPTS_PATH && {
+  cp /tmp/shell-utils-main/defaults/* $UTILS_SCRIPTS_PATH
+}
