d1c531c8951d4e50cb3b6d495c380c032de60841
Vinicius Mayrink <vmayrink12@gmail.com>
2025-02-16 08:45:40 -0300

    chore(installer): improves installation process

--- installer.sh
+++ installer.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-curl -L https://github.com/vncsmyrnk/shell-utils/archive/refs/heads/main.zip -o /tmp/shell-utils.zip
-unzip /tmp/shell-utils.zip -d /tmp
-\. /tmp/shell-utils-main/config/setup
-sudo install /tmp/shell-utils-main/bin/util $SU_BIN_PATH
-install /tmp/shell-utils-main/config/setup $SU_PATH
-mkdir -p $SU_SCRIPTS_PATH && {
-  cp /tmp/shell-utils-main/defaults/* $SU_SCRIPTS_PATH
+# Downloads shell-utils files and install them on
+# the expected directories
+
+PROJECT_NAME=shell-utils
+BRANCH=main
+
+curl -L "https://github.com/vncsmyrnk/$PROJECT_NAME/archive/refs/heads/$BRANCH.zip" \
+  -o "/tmp/$PROJECT_NAME.zip"
+unzip "/tmp/$PROJECT_NAME.zip" -d /tmp
+\. "/tmp/$PROJECT_NAME-$BRANCH/config/setup"
+sudo install -d "/tmp/$PROJECT_NAME-$BRANCH/bin/util" "$SU_BIN_PATH"
+install -d "/tmp/$PROJECT_NAME-$BRANCH/config/setup" "$SU_PATH"
+mkdir -p "$SU_SCRIPTS_PATH" && {
+  cp "/tmp/$PROJECT_NAME-$BRANCH/defaults/*" "$SU_SCRIPTS_PATH"
 }
