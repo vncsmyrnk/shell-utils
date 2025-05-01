1391f8f3f43854230d2c5012bc7d5632b2fee494
Vinicius Mayrink <vmayrink12@gmail.com>
2025-04-30 21:42:03 -0300

    chore(update): adds line breaks for util echos

--- defaults/update.sh
+++ defaults/update.sh
@@ -9,11 +9,11 @@ main() {
   if exists apt; then sudo apt-get update && sudo apt-get upgrade; fi
   if exists brew; then brew update && brew upgrade; fi
   if exists yay; then yay; fi
-  echo "[UTIL] Package managers OK\n"
+  echo "[UTIL] Package managers OK"
 
   # Checks for global update scripts on utils folder
   [ -x "$SU_SCRIPTS_ON_UPDATE_PATH" ] && {
-    echo "[UTIL] Now updating on-update scripts"
+    echo -e "\n[UTIL] Now updating on-update scripts"
     find $SU_SCRIPTS_ON_UPDATE_PATH \
       -type f \
       -follow \
@@ -24,7 +24,7 @@ main() {
 
   # Checks for global update script
   [ -x "$UPDATE_GLOBAL_SCRIPT" ] && {
-    echo "[UTIL] Now using the global update script"
+    echo -e "\n[UTIL] Now using the global update script"
     "$UPDATE_GLOBAL_SCRIPT"
   }
 }
