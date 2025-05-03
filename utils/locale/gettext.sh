923811f1fa3a44461c74f31c2d4ff42b050bd6cd
Vinicius Mayrink <vmayrink12@gmail.com>
2025-05-03 11:27:43 -0300

    feat(utils/locale): adds script that queries a translation in an mo file

---
+++ utils/locale/gettext.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Completions
+# -l[forces language instead of infering it from the mo path]
+
+while getopts ":l" opt; do
+  case $opt in
+    l) language="$OPTARG" ;;
+    \?) echo "Invalid option -$OPTARG" >&2 ;;
+  esac
+done
+
+path_to_mo_file="$1"
+message_id="$2"
+
+if [ -z "$path_to_mo_file" ] || [ -z "$message_id" ]; then
+  echo "usage: <path_to_mo_file> <message_id> -l <LANGUAGE>"
+  exit 1
+fi
+
+path_to_mo_file="$(realpath $path_to_mo_file)"
+if [ ! -f $path_to_mo_file ]; then
+  printf "file not found\n" >&2
+  exit 1
+fi
+
+dir_to_domain="$(dirname "$(dirname "$(dirname "$path_to_mo_file")")")"
+domain="$(basename "$path_to_mo_file" .mo)"
+
+if [ -z "$language" ]; then
+  language=$(basename "$(dirname "$(dirname "$path_to_mo_file")")")
+fi
+
+LANGUAGE="$language" TEXTDOMAIN="$domain" TEXTDOMAINDIR="$dir_to_domain" gettext "$message_id"
