918c821ca7c71e70d6aa6a290c66d3fcee31b08a
Vinicius Mayrink <vmayrink12@gmail.com>
2025-07-13 23:30:49 -0300

    feat(config): add scripts for adding and removing scripts folder to the util command

---
+++ defaults/config/add-script-folder.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# help: makes a folder of scripts acessible to the util command
+
+main() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: util config add-script-folder <folder> <destination_name>"
+    exit 1
+  fi
+
+  destination_path="$SU_SCRIPTS_PATH/$2"
+  mkdir -p "$destination_path"
+  stow -t "$destination_path" "$1" --no-folding
+}
+
+main "$@"
---
+++ defaults/config/remove-script-folder.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# help: removes a folder from the scripts acessible to the util command
+
+main() {
+  if [ -z "$1" ]; then
+    echo "Usage: util config remove-script-folder <folder>"
+    exit 1
+  fi
+
+  destination_path="$SU_SCRIPTS_PATH/$1"
+  stow -D -t "$destination_path" "$1" --no-folding
+  rm -rf "$destination_path"
+}
+
+main "$@"
