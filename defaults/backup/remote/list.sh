31ed598b7b28588e1a8eaf0d8538d24f10ad46c2
Vinicius Mayrink <vmayrink12@gmail.com>
2025-10-19 10:26:01 -0300

    fix(backup): adds `rclone` check when running the remote list script
    This avoids the `rclone: command not found` error.

--- defaults/backup/remote/list.sh
+++ defaults/backup/remote/list.sh
@@ -2,4 +2,9 @@
 
 # help: lists backups uploaded to the current set rclone remote
 
+if ! command -v rclone >/dev/null; then
+  echo "list failed: rclone not found." >&2
+  exit 1
+fi
+
 rclone ls $SU_SCRIPT_BACKUP_RCLONE_REMOTE:$SU_SCRIPT_BACKUP_RCLONE_FOLDER
