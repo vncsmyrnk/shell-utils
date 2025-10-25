bfcf77ab9978e7431e40ae585e054ae8cc310205
Vinicius Mayrink <vmayrink12@gmail.com>
2025-10-25 09:50:23 -0300

    docs(backup): moves generate command specific docs to its help section

--- extra/backup/generate.sh
+++ extra/backup/generate.sh
@@ -3,7 +3,11 @@
 # This script performs a backup of a previously
 # defined files
 
-# help: generates an encrypted backup file and uploads it to a rclone remote
+# help: Generates an encrypted backup file and uploads it to a rclone remote.
+#
+# All paths set at \033[1m$SU_BKP_PATHS\033[0m will be copied to the zip backup file. It is predefined with useful paths but it can be overriden.
+#
+# You must specify \033[1m$SU_SCRIPT_BACKUP_RCLONE_REMOTE\033[0m and \033[1m$SU_SCRIPT_BACKUP_RCLONE_FOLDER\033[0m in order to the backup be automatically uploaded to the remote.
 
 umask 077
 
