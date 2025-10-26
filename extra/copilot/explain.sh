9d8eefd19f229c6b08df46f83ef7c82211d0ce4e
Vinicius Mayrink <vmayrink12@gmail.com>
2025-10-26 16:44:20 -0300

    docs(extra)!: adds help sections for copilot commands

--- extra/copilot/explain.sh
+++ extra/copilot/explain.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# [help]
+# Uses GH Copilot CLI to explain something.
+#
+# Example: `util copilot explain how does the ln command work`
+
 gh copilot explain "$@"
--- extra/copilot/suggest.sh
+++ extra/copilot/suggest.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# [help]
+# Uses GH Copilot CLI to suggest commands.
+#
+# Example: `util copilot suggest how do I list and sort files by creation date`
+
 gh copilot suggest "$@"
