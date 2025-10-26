9cb85da12fcf56ab4916abeb956ce8e01e233369
Vinicius Mayrink <vmayrink12@gmail.com>
2025-10-26 16:11:21 -0300

    chore(setup)!: renames old `zsh` to be POSIX compliant and generic
    This implementation does not need to be shell specific and can be used
    in all shells.
    
    Completions is the only subject which needs a shell specific
    implementation.

--- defaults/cat/completions-zsh.sh
+++ defaults/cat/completions-zsh.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+
+# [help]
+# Sets the completions file for zsh.
+#
+# This enables completions for custom commands and options.
+#
+# Usage: `. <(util cat completions-zsh)`
+
 cat <<EOF
 [ -d "\$SU_COMPLETIONS_PATH" ] && fpath=("\$SU_COMPLETIONS_PATH" \$fpath)
 EOF
--- defaults/cat/setup-zsh.sh
+++
@@ -1,1 +0,0 @@
-cat $HOME/.config/util/zsh
