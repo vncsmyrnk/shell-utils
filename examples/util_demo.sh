f1c45e1fcd146da29990e584466afeb81255b342
Vinicius Mayrink <vmayrink12@gmail.com>
2026-03-01 09:22:02 -0300

    docs(examples): uses new `util config` features

--- examples/util_demo.sh
+++ examples/util_demo.sh
@@ -4,13 +4,9 @@ if ! command -v util; then
   echo "util not installed. Install it to run this demo."
 fi
 
-# Creates a folder under the scripts folder.
-# This will be the command.
-mkdir -p ~/.config/util/scripts/test
-
 # Creates a simple script which only displays.
 # a simple message. The script name will be the subcommand.
-cat <<EOF >~/.config/util/scripts/test/example.sh
+cat <<EOF >~/myscripts/example.sh
 #!/bin/sh
 # [help]
 # This is a test command. It only prints a "it works" message
@@ -19,14 +15,17 @@ echo "it works"
 EOF
 
 # Make it executable
-chmod u+x ~/.config/util/scripts/test/example.sh
+chmod u+x ~/myscripts/example.sh
+
+# Make it acessible to the util command
+util config add ~/myscripts/example.sh
 
 # Autocompletions can be defined at
-# ~/.config/util/scripts/test/example.completions.[shell] (i.e
-# ~/.config/util/scripts/test/example.completions.zsh).
+# ~/.config/util/scripts/example.completions.[shell] (i.e
+# ~/.config/util/scripts/example.completions.zsh).
 
 # Run it. It should run the script we just created.
-util test example
+util example
 
 # There you go. You can arrange your scripts in any folder
 # hierarchy you prefer. util will handle possible colisions.
