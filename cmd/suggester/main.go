fb24a32107d8867ae54f3210e4b0ca9f9e568d60
Vinicius Mayrink <vmayrink12@gmail.com>
2026-03-27 20:04:08 -0300

    fix(completions): adapts `suggester` to output precisely the needed suggestions and matches
    Now the autocompletion script uses `suggester` to search for completions
    to the util command.

--- cmd/suggester/main.go
+++ cmd/suggester/main.go
@@ -19,37 +19,62 @@ func main() {
 	}
 
 	configUserScriptsPath := filepath.Join(os.Getenv("HOME"), ".config", "shell-utils", "scripts")
-	pathGlob := fmt.Sprint(filepath.Join(os.Args[1:]...), "*")
-	for _, p := range []string{baseDefaultScriptsPath, configUserScriptsPath} {
-		path := pathGlob[:len(pathGlob)-1]
-		if m, err := filepath.Glob(filepath.Join(p, fmt.Sprint(path, ".*"))); err == nil && len(m) > 0 {
-			for _, exactPath := range m {
-				fmt.Println(filepath.Rel(p, exactPath))
-			}
-			return
-		}
+	scriptsLookupPaths := []string{baseDefaultScriptsPath, configUserScriptsPath}
 
-		if f, err := os.Stat(filepath.Join(p, path)); err == nil && f.IsDir() {
-			pathGlob = filepath.Join(path, "*")
-		}
-		entries, err := filepath.Glob(filepath.Join(p, pathGlob))
-		if err != nil {
-			fatalF("failed to read dir entries: %s", err)
-		}
-		for _, e := range entries {
-			fileName := filepath.Base(e)
-			if strings.HasPrefix(fileName, "_") || fileName == "help" {
+	validArgs := []string{""}
+	if len(os.Args) > 1 {
+		validArgs = strings.Split(os.Args[1], " ")
+	}
+
+	i := len(validArgs)
+	for pathGlob := fmt.Sprint(filepath.Join(validArgs...), "*"); i > 0; pathGlob = fmt.Sprint(filepath.Join(validArgs[:i]...), "*") {
+		i--
+
+		for _, p := range scriptsLookupPaths {
+			path := pathGlob[:len(pathGlob)-1] // Removes * to search for exact file path matches
+			if m, err := filepath.Glob(filepath.Join(p, fmt.Sprint(path, ".*"))); err == nil && len(m) > 0 {
+				for _, exactPath := range m {
+					stringArgs := strings.Join(validArgs[i+1:], " ")
+					if relP, err := filepath.Abs(exactPath); err == nil {
+						fmt.Print("match;", relP, ";", stringArgs, "\n") // Found exact match
+					}
+				}
+				return
+			}
+
+			if f, err := os.Stat(filepath.Join(p, path)); err == nil && f.IsDir() {
+				pathGlob = filepath.Join(path, "*") // Its a dir with a trailing space, list all entries inside it
+				if validArgs[i] != "" {
+					pathGlob = path // A path matching a dir without a trailing whitespace was detected, list only the dir itself
+				}
+			}
+			entries, err := filepath.Glob(filepath.Join(p, pathGlob))
+			if err != nil {
+				fatalF("failed to read dir entries: %s", err)
+			}
+			if len(entries) == 0 {
 				continue
 			}
-			noExtFileBaseName := strings.ReplaceAll(fileName, filepath.Ext(fileName), "")
-			fmt.Println(noExtFileBaseName)
+
+			i = 0 // Found files or dirs, break out after searching on all base paths
+			for _, e := range entries {
+				fileName := filepath.Base(e)
+				if strings.HasPrefix(fileName, "_") || fileName == "help" {
+					continue
+				}
+				noExtFileBaseName := strings.ReplaceAll(fileName, filepath.Ext(fileName), "")
+				fmt.Print("suggest;", noExtFileBaseName, "\n")
+			}
 		}
 	}
 }
 
 func help() {
 	fmt.Println("Suggester lists the autocompletion suggestions for the util command's arguments.")
-	fmt.Println("\nCheck out \033[4mhttps://github.com/vncsmyrnk/shell-utils\033[0m for more.")
+	fmt.Println("\nUsage:")
+	fmt.Println(" util-suggester <query>")
+	fmt.Println("\nThe query should be the exact prompt typed on the prompt when <TAB> is preesed.")
+	fmt.Println("Check out \033[4mhttps://github.com/vncsmyrnk/shell-utils\033[0m for more.")
 }
 
 func fatalF(s string, args ...any) {
