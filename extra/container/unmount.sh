81ec97f4735217af2be47529bc14add6e4a31dd8
Vinicius Mayrink <vmayrink12@gmail.com>
2026-05-07 10:46:57 -0300

    chore(extra/container): allows unmounting all containers from a directory
    All container files from a directory can be now simultaneously unmounted
    passing a directory to `util container unmount`.

--- extra/container/unmount.sh
+++ extra/container/unmount.sh
@@ -3,20 +3,38 @@
 # [help]
 # Umnounts a previously mounted container
 #
-# Usage: util container unmount <path/to/container>
+# Unmounts all containers from a directory if a directory is used.
+#
+# Usage: util container unmount <path/to/container/or/dir/of/containers>
 
-src="$1"
-if [[ -z "$src" ]]; then
-  exit 1
-fi
+unmount() {
+  src="$1"
+  dev=$(losetup -n -O NAME -j "$src" | head -n 1)
+  mapper=$(lsblk -nlo NAME "$dev" | tail -n 1)
 
-dev=$(losetup -n -O NAME -j "$src" | head -n 1)
-mapper=$(lsblk -nlo NAME "$dev" | tail -n 1)
+  udisksctl unmount -b "/dev/mapper/$mapper"
+  udisksctl lock -b "$dev"
 
-udisksctl unmount -b "/dev/mapper/$mapper"
-udisksctl lock -b "$dev"
+  dest="$HOME/$(basename "$src" | rev | cut -f2- -d "." | rev)"
+  rm -rf "$dest"
+  mkdir "$dest"
+  chmod 000 "$dest" # Avoids the poisoning of the destinatio
+}
 
-dest="$HOME/$(basename "$src" | rev | cut -f2- -d "." | rev)"
-rm -rf "$dest"
-mkdir "$dest"
-chmod 000 "$dest" # Avoids the poisoning of the destinatio
+main() {
+  src="$1"
+  if [[ -z "$src" ]]; then
+    exit 1
+  fi
+
+  if [[ -d "$src" ]]; then
+    for f in "$src"/*; do
+      unmount "$f"
+    done
+    exit 0
+  fi
+
+  unmount "$src"
+}
+
+main "$@"
