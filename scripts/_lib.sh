9228698ce0ec4bbf695ac0d9a15bc40fe7273479
Vinicius Mayrink <vmayrink12@gmail.com>
2026-06-07 15:47:24 -0300

    feat(scripts/update): updates Zsh's zinit if applicable
    resolves #92

--- scripts/_lib.sh
+++ scripts/_lib.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+BOLD='\033[1m'
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+ARROW="==>"
+
 _lib_files_filename_noext() {
   if ! name=$(basename "$1"); then
     echo "basename: $name" >&2
@@ -27,4 +33,27 @@ _lib_files_filename_noext() {
 _lib_fatal() {
   echo -e "$1" >&1
   exit 1
+}
+
+_lib_log_step_header() {
+  local no_upper_spacing=false
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    --no-upper-spacing)
+      no_upper_spacing=true
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      break
+      ;;
+    esac
+  done
+  if [[ "$no_upper_spacing" = false ]]; then
+    echo ""
+  fi
+  echo -e "${BOLD}${BLUE}${ARROW} $1 ${NC}"
 }
--- scripts/update.sh
+++ scripts/update.sh
@@ -4,13 +4,32 @@ set -e
 # [help]
 # Updates system packages
 
+: "${SHELL_UTILS_SCRIPTS_PATH:=}"
+# shellcheck source=scripts/_lib.sh
+\. "${SHELL_UTILS_SCRIPTS_PATH}/_lib.sh"
+
 main() {
+  _lib_log_step_header --no-upper-spacing "Updating system packages"
+
   # shellcheck disable=SC2310
   if exists apt; then sudo apt-get update && sudo apt-get upgrade; fi
   # shellcheck disable=SC2310
   if exists brew; then brew update && brew upgrade; fi
   # shellcheck disable=SC2310
   if exists yay; then yay --devel; fi
+
+  _lib_log_step_header "Updating shell environments"
+
+  # shellcheck disable=SC2310
+  if exists zsh; then
+    zsh -f -c '
+      ZINIT_HOME="$HOME/.local/share/zinit/zinit.git"
+      if [[ -f "${ZINIT_HOME}/zinit.zsh" ]] && [[ $(stat -c "%a" "${ZINIT_HOME}/zinit.zsh") == "644" ]]; then
+        source "${ZINIT_HOME}/zinit.zsh"
+        zinit self-update
+        zinit update --parallel
+      fi' 2>&1
+  fi
 }
 
 exists() {
