0b2bcd9aeab78e87eb3172c263d3c00bc3cb1e4c
Vinicius Mayrink <vmayrink12@gmail.com>
2026-06-07 14:44:48 -0300

    fix(scripts/workspace): corrects containers lib source path

--- scripts/workspaces.ws/unset.sh
+++ scripts/workspaces.ws/unset.sh
@@ -14,7 +14,7 @@ set -e
 \. "${SHELL_UTILS_SCRIPTS_PATH}/_lib.sh"
 
 # shellcheck source=scripts/containers.cont/_lib.sh
-\. "${SHELL_UTILS_SCRIPTS_PATH}/containers/_lib.sh"
+\. "${SHELL_UTILS_SCRIPTS_PATH}/containers.cont/_lib.sh"
 
 : "${SHELL_UTILS_SCRIPT_DIRNAME:=}"
 # shellcheck source=scripts/workspaces.ws/_variables.sh
