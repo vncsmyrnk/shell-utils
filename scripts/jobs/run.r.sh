746bd4f4dafbac50af55423f6881ea3e7fd9186c
Vinicius Mayrink <vmayrink12@gmail.com>
2026-06-07 19:40:31 -0300

    feat(scripts/jobs): creates a job from a file via `-f|--file` flag

--- scripts/jobs/run.r.sh
+++ scripts/jobs/run.r.sh
@@ -4,15 +4,54 @@ set -e
 # [help]
 # Executes a command on a new window in the default session
 #
-# Usage: util jobs run [NAME] [COMMAND]
+# Usage: util jobs run <NAME> <COMMAND> [OPTIONS]
+#
+# Options:
+#  -f, --file   Run executable file as a job
+
+: "${SHELL_UTILS_SCRIPTS_PATH:=}"
+# shellcheck source=scripts/_lib.sh
+\. "${SHELL_UTILS_SCRIPTS_PATH}/_lib.sh"
 
 : "${SHELL_UTILS_SCRIPT_DIRNAME:=}"
 # shellcheck source=scripts/jobs/_variables.sh
 \. "${SHELL_UTILS_SCRIPT_DIRNAME}/_variables.sh"
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  -f | --file)
+    file="$2"
+    shift 2
+    ;;
+  --file=*)
+    file="${1#*=}"
+    shift
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
 job_name="$1"
-shift
-job_task="$*"
+if [[ -x "$file" ]]; then
+  if [[ -z "$job_name" ]]; then
+    job_name=$(
+      set -e
+      _lib_files_filename_noext "$file"
+    )
+  fi
+  job_task=$(realpath "$file")
+else
+  if [[ -z "$job_name" ]] || ! shift || [[ -z "$*" ]]; then
+    _lib_fatal "required arguments absent, run \033[1mutil jobs run --help\033[0m to see usage details"
+  fi
+  job_task="$*"
+fi
 
 if [[ -z "$job_task" ]]; then
   exit 1
@@ -38,6 +77,8 @@ if ! tmux list-windows -t "$_jobs_session_name" >/dev/null 2>&1; then
 fi
 
 tmux_windows=$(tmux list-windows -t "$_jobs_session_name" -F "#{m:$job_name,#{window_name}}")
-grep -q 1 <<<"$tmux_windows"
+if grep -q 1 <<<"$tmux_windows"; then
+  _lib_fatal "an existing job already exist with this name."
+fi
 
 tmux new-window -t "$_jobs_session_name" -n "$job_name" "$job_exec"
