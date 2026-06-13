75c5bd82539f16c5ee4289ca2299d9c69215f134
Vinicius Mayrink <vmayrink12@gmail.com>
2026-06-13 11:48:27 -0300

    feat(scripts/workspaces): allows removing specific paths before `workspaces set`
    It now supports config defined on
    `$HOME/.config/shell-utils/workspaces/<name>.json`.
    
    The paths defined on `.removeBeforeStow` are removed interactively
    before stowing. Environment variables are resolved via `envsubst`.
    
    resolves #95

--- scripts/workspaces.ws/_variables.sh
+++ scripts/workspaces.ws/_variables.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 _workspaces_mount_path="/tmp/shell-utils.$(whoami)"
+_workspaces_config_dir="$HOME/.config/shell-utils/workspaces"
--- scripts/workspaces.ws/set.sh
+++ scripts/workspaces.ws/set.sh
@@ -34,6 +34,11 @@ _ssh_key_add() {
   ssh-add "$key"
 }
 
+_config() {
+  : "${_workspaces_config_dir:=}"
+  cat "$_workspaces_config_dir/$1.json"
+}
+
 main() {
   local src="$1"
   if [[ -z "$src" ]]; then
@@ -57,10 +62,35 @@ main() {
 
   _container_mount "$src" "$target_name" "$target"
 
+  # shellcheck disable=SC2310,SC2311
+  if config=$(_config "$target_name" 2>/dev/null) && paths_to_remove=$(jq -cr '.removeBeforeStow[]' <<<"$config" 2>/dev/null); then
+    paths=()
+    while IFS= read -u 3 -r item; do
+      p=$(envsubst <<<"$item")
+      if [[ -e "$p" ]]; then
+        paths+=("$p")
+      fi
+    done 3<<<"$paths_to_remove"
+
+    if [[ "${#paths}" -gt 0 ]]; then
+      echo "conflicting paths exist:"
+      printf -- '- %s\n' "${paths[@]}"
+      printf "\nremove paths and procceed? (y/N) "
+      read -r answer
+      if [[ ! "$answer" =~ ^([Yy])$ ]]; then
+        _container_unmount "$target_name" "$target"
+        return 1
+      fi
+      for p in "${paths[@]}"; do
+        rm -r "$p"
+      done
+    fi
+  fi
+
   local stow_target="$HOME"
   if ! stow -d "$target" -t "$stow_target" .; then
     _container_unmount "$target_name" "$target"
-    exit 1
+    return 1
   fi
 
   _ssh_key_add
