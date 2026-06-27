3c919817665c575e2a94cca7b80fee0719b039ae
Vinicius Mayrink <vmayrink12@gmail.com>
2026-06-27 09:35:34 -0300

    chore(scripts/tmux)!: selects the initial pane after split
    The old behavior is still available with `--select-last-pane`.
    
    resolves #112

--- scripts/tmux.tmx/split.sp.sh
+++ scripts/tmux.tmx/split.sp.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
-set -e
+set -eou pipefail
 
 # [help]
 # Splits the current window both horizontally and vertically
+#
+# Usage: util tmux split [FLAGS]
+#
+# Options:
+#  --select-last-pane   Selects the last pane after split
 
-tmux split-window -h &&
-  tmux split-window -v
+select_last_pane=false
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --select-last-pane)
+    select_last_pane=true
+    shift
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
+if ! current_pane_id=$(tmux list-panes -F '#{pane_id}' -f '#{m:1,#{pane_active}}'); then
+  echo "failed to fetch current pane id, selecting current pane will not work." >&2
+fi
+
+tmux split-window -h
+tmux split-window -v
+
+if [[ "$select_last_pane" = false ]]; then
+  tmux select-pane -t "$current_pane_id"
+fi
