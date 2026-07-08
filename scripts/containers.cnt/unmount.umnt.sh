3fe5c3a9ac4faac14ca1639b91af7704a6937ac0
Vinicius Mayrink <vmayrink12@gmail.com>
2026-07-07 21:10:41 -0300

    feat(scripts/containers): allows forcefully killing processes associated with a container when unmounting
    Defaults to prompt [y/N] and supports `-f|--force`.
    
    resolves #113

--- scripts/containers.cnt/unmount.umnt.sh
+++ scripts/containers.cnt/unmount.umnt.sh
@@ -19,9 +19,31 @@ set -e
 
 : "${_containers_target_name_prefix:=}"
 
-src="$1"
+src=
+force=false
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  -f | --force)
+    force=true
+    shift
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    if [[ -n "$src" ]]; then
+      echo "Unexpected extra argument \"$1\""
+      break
+    fi
+    src="$1"
+    shift
+    ;;
+  esac
+done
+
 if [[ ! -f "$src" ]]; then
-  src="$_containers_image_dir/$1.img"
+  src="$_containers_image_dir/$src.img"
   if [[ ! -f "$src" ]]; then
     _lib_fatal "container file not found."
   fi
@@ -31,6 +53,23 @@ target=$(
   set -e
   _container_mounted "$src"
 )
+
+# shellcheck disable=SC2310,SC2311
+if ! target=$(_container_mounted "$src"); then
+  echo "$target" >&2
+  exit 1
+fi
+
+if fuser -s -m "$target"; then
+  if [[ "$force" = false ]]; then
+    fuser -v -m "$target"
+    read -r -p "kill and procceed? (y/N) " answer
+    if [[ ! "$answer" =~ ^([Yy])$ ]]; then
+      exit 1
+    fi
+  fi
+  fuser -s -m "$target" -k
+fi
 
 target_name=$(
   set -e
